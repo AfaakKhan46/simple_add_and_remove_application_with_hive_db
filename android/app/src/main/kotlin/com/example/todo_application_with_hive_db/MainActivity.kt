1a065f5e00e8a387ad8419b04ac2cf4d00c0b47b
Talha_khan <Afaak464@gmail.com>
2023-07-25 04:29:10 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/todo_application_with_hive_db/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.todo_application_with_hive_db
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Box friendBox = Hive.box('friend');
+  String? name;
+
+  addFriend()async{
+    await friendBox.put('name', "Afaq Khan");
+
+  }
+  getFriend()async{
+    setState(() {
+      name = friendBox.get('name');
+    });
+
+  }
+  updateFriend()async{
+    await friendBox.put('name', "khan Afaq");
+
+  }
+  deleteFriend()async{
+    await friendBox.delete('name');
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hive DB'),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("$name"),
+            ElevatedButton(onPressed: addFriend, child: Text('Create')),
+            ElevatedButton(onPressed: getFriend, child: Text('Read')),
+            ElevatedButton(onPressed: updateFriend, child: Text('Update')),
+            ElevatedButton(onPressed: deleteFriend, child: Text('Delete')),
+          ],
+        ),
+      ),
+    );
+  }
+}
